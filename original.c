d02f38f87c09de9fd673834ceafaf55c55768890
kobaba5858 <113877034+kobaba5858@users.noreply.github.com>
2022-11-12 12:41:59 +0900

    Revert "とりあえず最後までコードは入力。しかし、迷路表示がずれている。ESCキーで初期位置に移動できない。反応が遅い点は改良必要。"

--- original.c
+++ original.c
@@ -8,42 +8,17 @@
 #include<termios.h>
 #include<unistd.h>
 #include<fcntl.h>
+#include"kbhit.h" 
 
 #define GYO 10  // 迷路の行数
 #define RETU 10 // 迷路の列数
-
- int kbhit(void)
-{
-    struct termios oldt, newt;
-    int ch;
-    int oldf;
-
-    tcgetattr(STDIN_FILENO, &oldt);
-    newt = oldt;
-    newt.c_lflag &= ~(ICANON | ECHO);
-    tcsetattr(STDIN_FILENO, TCSANOW, &newt);
-    oldf = fcntl(STDIN_FILENO, F_GETFL, 0);
-    fcntl(STDIN_FILENO, F_SETFL, oldf | O_NONBLOCK);
-
-    ch = getchar(); 
-
-    tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
-    fcntl(STDIN_FILENO, F_SETFL, oldf);
-
-    if (ch != EOF)
-    {
-        ungetc(ch, stdin);
-        return 1;
-    }
-	return 0;
-} 
 
 // 迷路マップデータ
 int meiro[GYO][RETU] = {
     {1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
     {1, 0, 0, 0, 0, 0, 1, 0, 0, 1},
     {1, 1, 1, 1, 1, 0, 1, 0, 0, 1},
-    {1, 0, 0, 0, 0, 0, 1, 0, 0, 1},
+    {1, 0, 0, 0, 0, 1, 1, 0, 0, 1},
     {1, 0, 0, 0, 0, 0, 1, 0, 0, 1},
 	{1, 0, 0, 0, 1, 1, 1, 0, 0, 1},
 	{1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
@@ -56,32 +31,13 @@ int px, py; // プレイヤーのxy座標
 int goal_count; // 塗りつぶすべき床の数
 int count;  //  塗りつぶした床の数
 
-// マップ表示
-void display_meiro(void){
-    int x, y;
-    for(y=0; y<GYO; y++){
-        for(x=0; x<RETU; x++){
-            if(x == px && y == py){ //  プレイヤーの位置のとき
-                meiro[y][x] = 2;    //  塗りつぶす
-                count++;            //  塗りつぶした数をカウント
-                printf("人");       //  プレイヤー
-            }
-            else if(meiro[y][x] == 0)   //  移動可能な床
-                printf("　");           //  全角スペース
-            else if(meiro[y][x] == 1)   //  壁
-                printf("■");
-            else if(meiro[y][x] == 2)   //塗った床
-                printf("ｘ");
-        }
-        printf("\n");
-    }
-    printf("move: ←↑→↓ restart: ESC\n");    //  操作説明
-}
-
 // 初期状態に戻る
-void return_start(void){
+void play_start(void){
     int x, y;
-        for(y=0; y<GYO; y++)
+    count = 0;
+    px = 1;
+    py = 1;
+    for(y=0; y<GYO; y++)
         for(x=0; x<RETU; x++)
             if(meiro[y][x] == 2){
                 meiro[y][x] = 0;   // 塗りつぶした床をもとに戻す
@@ -95,11 +51,31 @@ void goal_count_check(void){
     for(y=0; y<GYO; y++)
         for(x=0; x<RETU; x++)
             if(meiro[y][x] == 0){
-                goal_count++;   // マップ上の0(移動可能場所)をカウント
+                goal_count++;   // 移動可能な床の数をカウントする
             }
 }
 
-
+// 迷路を描く
+void draw_meiro(void){
+    int x, y;
+    for(y=0; y<GYO; y++){
+        for(x=0; x<RETU; x++){
+            if(x == px && y == py){ //  プレイヤーの位置のとき
+                meiro[y][x] = 2;    //  塗りつぶす
+                count++;            //  塗りつぶした数をカウント
+                printf("人");       //  プレイヤー
+            }
+            else if(meiro[y][x] == 0)   //  移動可能な床
+                printf("　");           //  全角スペース
+            else if(meiro[y][x] == 1)   //  壁
+                printf("■");
+            else if(meiro[y][x] == 2)   //塗った床
+                printf("x");
+        }
+        printf("\n");
+    }
+    prinff("move: ←↑→↓ restart: ESC\n");    //  操作説明
+}
 
 //  キー入力
 void key_input(void){
@@ -110,16 +86,16 @@ void key_input(void){
             break;
         }
     }
-    if(key == 119 && meiro[py-1][px] == 0)          /* wキー */
+    if(key == && meiro[py-1][px] == 0)          /* ↑キー */
         py --;  /* 上に移動 */
-    else if(key == 115 && meiro[py+1][px] == 0)     /* sキー */
+    else if(key == && meiro[py+1][px] == 0)     /* ↓キー */
         py ++;  /* 下に移動 */
-    else if(key == 97 && meiro[py][px-1] == 0)     /* aキー */
+    else if(key == && meiro[py][px-1] == 0)     /* ←キー */
         px --;  /* 左に移動 */
-    else if(key == 100 && meiro[py][px+1]== 0)      /* dキー */
+    else if(key == && meiro[py][px+1]== 0)      /* →キー */
         px ++;  /* 右に移動 */
-    else if(key == 27)                            /* ESCキー */
-        return_start();       /* 最初の状態に戻る */
+    else if(key == )                            /* ESCキー */
+        play_start();       /* 最初の状態に戻る */
     else                    /* 上記以外のキーの場合 */
         key_input();        /* 再度キー入力受付 */
 }
@@ -133,8 +109,8 @@ int main(void){
 
 /* ゲームループ */
     while(1){
-        system("clear");      /* コンソール画面をクリア */
-        display_meiro();       /* 迷路を表示 */
+        system("cls");      /* コンソール画面をクリア */
+        draw_meiro();       /* 迷路を表示 */
 
         if(count == goal_count){        /* 床を全て塗りつぶしたかのチェック */
         printf("全て塗りました！\n");
