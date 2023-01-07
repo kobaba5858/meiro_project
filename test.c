930f821f4c1d580ccfef0bdc33a65551d1102998
bashikoban1227@gmail.com <kobamac@kobayashiyuutanoiMac.local>
2023-01-07 14:57:31 +0900

    テスト2023/01/07

--- test.c
+++ test.c
@@ -1,31 +1,35 @@
 #include<stdio.h>
 
-int main(void){
-  int x, y;
-  int meiro[10][10];
-  
-  FILE *test;
+#define WHIDTH 5  // xで管理
+#define HEIGHT 5  // yで管理
 
-  test = fopen("test.txt","r");
-  
-  if(test == NULL){
+int main(void){
+    int x, y;
+    int meiro[HEIGHT][WHIDTH];
+
+    FILE *test;
+
+    test = fopen("test.txt","r");
+
+    if(test == NULL){
     printf("ファイルを開けませんでした。\n");
     return 1;
-  }
-
-  for(y = 0; y<10; y++){
-    for(x = 0; x<10; x++){
-      fscanf(test, "%d", &meiro[y][x]);
     }
-  }
 
-  fclose(test);
+    for(y = 0; y < HEIGHT; y++){
+      for(x = 0; x < WHIDTH; x++){
+        fscanf(test, "%d", &meiro[y][x]);
+      }
+    }
 
-  for(y = 0; y<10; y++){
-     for(x = 0; x<10; x++){
-      printf("%d", meiro[y][x]);
-     }
-     printf("\n");
-   }
-  return 0;
+    fclose(test);
+
+    for(y = 0; y < HEIGHT; y++){
+      for(x = 0; x < WHIDTH; x++){
+        printf("test[%d][%d] = %d ", y, x, meiro[y][x]);
+      }
+      printf("\n");
+    }
+    
+    return 0;
 }
